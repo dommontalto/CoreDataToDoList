7750d5d293787e308fd9562e9b1dca7f444cf109
Dom Montalto <dmontalto@imperosoftware.com>
2025-03-21 08:53:50 +0000

    Core Data

---
+++ CoreDataToDoList/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  CoreDataToDoList
+//
+//  Created by Dominic Montalto on 21/03/2025.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
